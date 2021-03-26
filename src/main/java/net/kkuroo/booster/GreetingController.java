e2e622b123112d03bc3320c0770960dd2b1e7afc
Dennis.Koo <kkuroo@gmail.com>
2021-03-27 00:38:26 +0900

    added method for raising exception

--- src/main/java/net/kkuroo/booster/GreetingController.java
+++ src/main/java/net/kkuroo/booster/GreetingController.java
@@ -16,4 +16,9 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+	@GetMapping("/except")
+	public void except() throws Exception {
+		float i = 2 / 0;
+	}
 }
